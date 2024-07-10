fb06f8f56bca819f3bf81fa06db0cf7f36ae73b0
Cristian <cristian.borodin@undelucram.md>
2024-07-10 17:50:10 +0300

    Ruff reformatted

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,25 @@
 from flask import Flask
 from flask_restx import Api
 
+
 def create_app():
-    app = Flask(__name__)
+	app = Flask(__name__)
 
-    from . import routes
-    from . import cli
+	from . import routes
+	from . import cli
 
-    api = Api(app, version='1.0', title='My API', description='A simple API', doc='/swagger')
-    
-    # API namespace
-    api.add_namespace(routes.api_namespace)
+	api = Api(
+		app,
+		version='1.0',
+		title='My API',
+		description='A simple API',
+		doc='/swagger',
+	)
 
-    # CLI commands
-    app.cli.add_command(cli.show_command)
+	# API namespace
+	api.add_namespace(routes.api_namespace)
 
-    return app
+	# CLI commands
+	app.cli.add_command(cli.show_command)
+
+	return app
